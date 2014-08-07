34cae4ac81798126d0a7f945ebd42adb58bca313
Srinivasulu Punuru <srip@microsoft.com>
2014-08-07 07:51:29 -0700

    Initial Commit

---
+++ ResourcePool/ResourcePool.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnBig
+{
+    /// <summary>ResourcePool class</summary>
+    /// <typeparam name="T"></typeparam>
+    public class ResourcePool<T>
+    {
+        /// <summary>MaxResourcePoolLength data member</summary>
+        private const int MaxResourcePoolLength = 10000;
+
+        /// <summary>availableResources data member</summary>
+        private Queue<T> availableResources;
+
+        /// <summary>reservedResources data member</summary>
+        private HashSet<T> reservedResources = new HashSet<T>();
+
+        /// <summary>Dummy object that is used to ensure 
+        /// releaseResourceLock executes exclusively.</summary>
+        private object releaseResourceLock = new object();
+
+        /// <summary>Initializes a new instance of the ResourcePool{T} class</summary>
+        /// <param name="resources">resources to use</param>
+        public ResourcePool(T[] resources)
+        {
+            if(resources == null)
+            {
+                throw new ArgumentNullException("resources");
+            }
+            else if(resources.Length == 0)
+            {
+                throw new ArgumentException("There are no entries in the resources", "resources");
+            }
+            else if(resources.Length > MaxResourcePoolLength)
+            {
+                throw new ArgumentException(
+                    "The resources count is more than the maximum resources that can be added to the resource pool",
+                    "resources");
+            }
+            
+            this.availableResources = new Queue<T>(resources);            
+        }
+
+        /// <summary>Gets the resource</summary>
+        /// <returns>resulting value</returns>
+        public T GetResource()
+        {          
+            if(this.availableResources.Count == 0)
+            {
+                throw new InvalidOperationException("No resources are available in the pool.");
+            }
+
+            T dequeuedResource = this.availableResources.Dequeue();
+            this.reservedResources.Add(dequeuedResource);
+            return dequeuedResource;
+        }
+
+        /// <summary>Releases the resource</summary>
+        /// <param name="resource">resource to use</param>
+        public void ReleaseResource(T resource)
+        {
+            if(resource == null)
+            {
+                throw new ArgumentNullException("resource to be released cannot be null", "resource");
+            }
+
+            // This lock is to prevent the race conditions when the two threads try to release the same resource
+            // at the same time, a rare but possible scenario
+            lock(releaseResourceLock)
+            {
+                if (this.reservedResources.Contains(resource))
+                {
+                    this.reservedResources.Remove(resource);
+                    this.availableResources.Enqueue(resource);
+                }
+                else
+                {                    
+                    throw new InvalidOperationException("The resource cannot be released.");
+                }
+            }
+        }
+    }
+}
---
+++ ResourcePoolTest/ResourcePoolTest.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnBig;
+
+namespace LearnBig
+{
+    [TestClass]
+    public class ResourcePoolTest
+    {
+        private int[] resourcesSample;
+
+        public ResourcePoolTest()
+        {
+            this.resourcesSample = this.GetResources(10000);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_ShouldThrow_WhenArgumentIsNull()
+        {            
+            // Act
+            var resourcePool = new ResourcePool<int>(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_ShouldThrow_WhenResourcesArgumentContainsZeroElements()
+        {
+            // Act
+            var resourcePool = new ResourcePool<int>(new int[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_ShouldThrow_WhenResourcesArgumentContainsMoreThanMaximumNumberOfResourcePoolElements()
+        {
+            // Act
+            var resourcePool = new ResourcePool<int>(this.GetResources(10001));
+        }
+
+        [TestMethod]
+        public void GetResource_ShouldReturnResourceFromThePool()
+        {
+            // Arrange
+            int[] resources = this.resourcesSample;
+            var resourcePool = new ResourcePool<int>(resources);
+
+            // Act
+            int resource = resourcePool.GetResource();
+
+            // Assert
+            Assert.IsTrue(resources.Any(r => r == resource));
+        }
+
+        [TestMethod]
+        public void GetResource_ShouldNotReturnElementWhichIsAlreadyReserved_WhenCalledMultipleTimes()
+        {
+            // Arrange
+            int[] resources = this.resourcesSample;            
+            var resourcePool = new ResourcePool<int>(resources);
+
+            HashSet<int> resourcesReturnedAlready = new HashSet<int>();
+            for(int index = 0; index < resources.Length; index++)
+            {
+                // Act            
+                int resource = resourcePool.GetResource();
+                
+                // Assert
+                Assert.IsFalse(resourcesReturnedAlready.Contains(resource));
+            }         
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReleaseResource_ShouldThrow_WhenResourceIsNull()
+        {
+            // Arrange
+            var resourcePool = new ResourcePool<object>(new object[4]);
+            // Act
+            resourcePool.ReleaseResource(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ReleaseResource_ShouldThrow_WhenReleasingResourceThatIsNotReserved()
+        {
+            // Arrange
+            int[] resources = this.resourcesSample;
+            var resourcePool = new ResourcePool<int>(resources);
+            
+            // Act
+            resourcePool.ReleaseResource(resources[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void GetResource_ShouldThrow_WhenAllResourcesAreReleased()
+        {
+            // Arrange
+            int[] resources = this.resourcesSample;
+            var resourcePool = new ResourcePool<int>(resources);
+            for(int index = 0; index < resources.Length; index++)
+            {
+                try
+                {
+                    resourcePool.GetResource();
+                }
+                catch(InvalidOperationException)
+                {
+                    // The above GetResource method shouldn't throw InvalidOperationException
+                    // If it throws then the test should fail.
+                    Assert.Fail("GetResource threw InvalidOperationException at an unexpected place.");
+                }
+            }
+
+            // Act
+            resourcePool.GetResource();
+        }
+
+        [TestMethod]
+        public void ReleaseResource_ShouldMakeResourceAvailableForOthers()
+        {
+            // Arrange
+            int[] resources = this.resourcesSample;
+            var resourcePool = new ResourcePool<int>(resources);
+            int resource = resourcePool.GetResource();
+            resourcePool.ReleaseResource(resource);
+            bool isReleasedResourceInPool = false;
+            
+            // Act
+            for(int index = 0; index < resources.Length; index++)
+            {
+                int resource2 = resourcePool.GetResource();
+                if(resource == resource2)
+                {
+                    isReleasedResourceInPool = true;
+                }
+            }
+
+            Assert.IsTrue(isReleasedResourceInPool);
+        }
+
+        private int[] GetResources(int resourceCount)
+        {
+            Random rnd = new Random();
+            int[] retVal = new int[resourceCount];
+            for(int index = 0; index < resourceCount; index ++)
+            {
+                retVal[index] = rnd.Next(1, resourceCount);
+            }
+
+            return retVal;
+        }
+    }
+}
